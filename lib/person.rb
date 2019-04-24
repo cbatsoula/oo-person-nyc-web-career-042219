182ac95a4510caa27a5c54f2790b5ce4526eb928
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-24 16:57:13 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,30 @@
-# your code goes here
+require 'pry'
+
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+ 
+ def happiness=(happiness_level)
+   @happiness = happiness_level
+   @happiness = 10 if happiness_level > 10
+   
+   @happiness = 0 if happiness_level < 0
+ end
+
+  def hygiene=(hygiene_level)
+    @hygiene = hygiene_level 
+    @hygiene = 10 if hygiene_level > 10
+    @hygiene = 0 if hygiene_level < 0
+  end
+   
+ end
+  
+
+
